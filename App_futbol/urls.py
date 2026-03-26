068a8aca630e2ea82c9000c96f723c0650575267
Miguel Angel Rojas Gonzalez <miguelangel.rojasg@gmail.com>
2026-03-26 00:39:03 -0300

    Los archivos html funcionan, ahora estamos en los paso de armado de lista

--- App_futbol/urls.py
+++ App_futbol/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("jugadores.urls")),
+    path("", include("lista_jugador.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
